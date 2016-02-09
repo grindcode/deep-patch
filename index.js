9a3fafb2605532fcea7b6aabec4f3968a31320b7
Isis T. Baulig <grindcode@gmail.com>
2016-02-09 18:58:05 +0100

    fix styles & naming

--- index.js
+++ index.js
@@ -5,57 +5,54 @@ var concat = require('lodash.concat')
 var traverse = require('traverse')
 /**
  * Modify an object recursively by an array of sequential patches.
- * @param {Object} obj - Object input.
+ * @param {Object} input - Object input.
  * @param {Array} mods - Array of patches.
  * @returns {Object} Resulting object.
  */
-module.exports = function (obj, mods) {
-  if (typeof obj !== 'object')
+module.exports = function (input, patches) {
+  if (typeof input !== 'object')
     throw new TypeError('Invalid first argument. Object is expected.')
-  if (!Array.isArray(mods))
+  if (!Array.isArray(patches))
     throw new TypeError('Invalid second argument. Array is expected.')
-  var apply = function (source, mod) {
-    var traversedMod = traverse(mod)
+  var apply = function (source, patch) {
+    var traversedPatch = traverse(patch)
     var traversedSource = traverse(source)
+    var getSource = function (path) {
+      return traversedSource.get(path)
+    }
     var transformNode = function (node) {
       var isOperation = function (name) {
         return node.hasOwnProperty(name)
       }
-      var getSource = function () {
-        return traversedSource.get(this.path)
-      }
-
-      if (typeof node !== 'object')
+      if (typeof node !== 'object') {
         return node
-
-      if (isOperation('$set'))
+      }
+      if (isOperation('$set')) {
         return node.$set
-
+      }
       if (isOperation('$push')) {
-        var sourceNode = getSource()
+        var sourceNode = getSource(this.path)
         return (sourceNode)? concat(sourceNode, [node.$push]): [node.$push]
       }
-
       if (isOperation('$unshift')) {
-        var sourceNode = getSource()
+        var sourceNode = getSource(this.path)
         return (sourceNode)? concat([node.$unshift], sourceNode): [node.$unshift]
       }
-
-      if (isOperation('$filter'))
-        return getSource().filter(node.$filter)
-
-      if (isOperation('$map'))
-        return getSource().map(node.$map)
-
-      if (isOperation('$apply'))
-        return node.$apply.call(source, getSource())
-
-      if (isOperation('$merge'))
-        return merge(getSource(), node.$merge)
-
+      if (isOperation('$filter')) {
+        return getSource(this.path).filter(node.$filter)
+      }
+      if (isOperation('$map')) {
+        return getSource(this.path).map(node.$map)
+      }
+      if (isOperation('$apply')) {
+        return node.$apply.call(source, getSource(this.path))
+      }
+      if (isOperation('$merge')) {
+        return merge(getSource(this.path), node.$merge)
+      }
       return node
     }
-    return assign({}, source, traversedMod.map(transformNode))
+    return assign({}, source, traversedPatch.map(transformNode))
   }
-  return reduce(mods, apply, obj)
+  return reduce(patches, apply, input)
 }
